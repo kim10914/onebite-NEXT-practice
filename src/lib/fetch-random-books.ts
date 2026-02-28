09dcebe8013d4eef48fd635cca7ae80b5d6c9936
kim10914 <43512783+kim10914@users.noreply.github.com>
2026-03-01 05:30:30 +0900

    fix : 성공에서 왜 오류를 던지도록 해 놓은 거시야?

--- src/lib/fetch-random-books.ts
+++ src/lib/fetch-random-books.ts
@@ -1,16 +1,15 @@
 import { BookData } from "@/types";
 export default async function fetchRandomBooks(): Promise<BookData[]> {
-    const url = "http://localhost:12345/book/random";
+  const url = "http://localhost:12345/book/random";
 
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            throw new Error();
-        }
-        return await response.json();
-    } catch (err) {
-        console.log(err);
-        return [];
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error();
     }
-
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 }
